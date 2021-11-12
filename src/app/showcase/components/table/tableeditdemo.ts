8b2ad3593f4c45e74f70924b27996edabe53829a
Fiona Tran <fiona.tran@ixup.com>
2021-11-12 11:47:47 +1100

    Add delete to the Cell editing with reorder table

--- src/app/showcase/components/table/tableeditdemo.ts
+++ src/app/showcase/components/table/tableeditdemo.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit} from '@angular/core';
 import { Product } from '../../domain/product';
 import { ProductService } from '../../service/productservice';
-import { SelectItem } from 'primeng/api';
+import { ConfirmationService, SelectItem } from 'primeng/api';
 import { MessageService } from 'primeng/api';
 
 @Component({
     templateUrl: './tableeditdemo.html',
-    providers: [MessageService],
+    providers: [MessageService, ConfirmationService],
     styles: [`
         :host ::ng-deep .p-cell-editing {
             padding-top: 0 !important;
@@ -28,7 +28,10 @@ export class TableEditDemo implements OnInit {
 
     clonedProducts: { [s: string]: Product; } = {};
 
-    constructor(private productService: ProductService, private messageService: MessageService) { }
+    constructor(
+        private productService: ProductService,
+        private messageService: MessageService, 
+        private confirmationService: ConfirmationService) { }
 
     ngOnInit() {
         this.productService.getProductsSmall().then(data => this.products = data);
@@ -44,12 +47,26 @@ export class TableEditDemo implements OnInit {
         ];
     }
 
+    onDelete(product: Product) {
+        this.confirmationService.confirm({
+            message: 'Are you sure you want to delete ' + product.name + '?',
+            header: 'Confirm',
+            icon: 'pi pi-exclamation-triangle',
+            accept: () => {
+                this.products = this.products.filter(val => val.id !== product.id);
+                console.log('this.products in delete', this.products);
+            }
+        });
+    }
+
     onEditComplete (event) {
         console.log(event);
+        console.log('products edit', this.products);
     }
 
     onRowReorder(event) {
         console.log(event);
+        console.log('products reordered', this.products);
     } 
 
     onRowEditInit(product: Product) {
